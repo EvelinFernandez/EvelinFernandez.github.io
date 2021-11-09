975e78ee8b4413d7a6b4795bf7b0e6fc9184f75a
EvelinFernandez <72705359+EvelinFernandez@users.noreply.github.com>
2021-11-08 17:51:14 -0700

    ptweet

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -2,8 +2,6 @@ window.onload=()=>{
     let usuario=JSON.parse(localStorage.getItem("usuario"));
     document.getElementById("imgUser").src="./img/"+usuario.imagen;
     let arrayTw=[];
-    //console.log(usuario);
-    //document.getElementById("usuario").innerHTML="Hola "+usuario.nombre;
     let txtTweet=document.getElementById("txtTweet");
     let btnTweet=document.getElementById("btnTweet");
     btnTweet.addEventListener('click',(evt)=>{
