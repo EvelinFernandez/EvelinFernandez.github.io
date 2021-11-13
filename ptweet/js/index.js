33a63e556b761ef6162f5699b8eb35baf7758997
EvelinFernandez <72705359+EvelinFernandez@users.noreply.github.com>
2021-11-12 23:34:25 -0700

    ptweet

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -2,6 +2,19 @@ window.onload=()=>{
     let usuario=JSON.parse(localStorage.getItem("usuario"));
     document.getElementById("imgUser").src="./img/"+usuario.imagen;
     let arrayTw=[];
+   
+    var xobj= new XMLHttpRequest();
+    xobj.overrideMimeType("application/js");
+    xobj.open('GET', './js/tweet.json',true);
+    xobj.onreadystatechange=function(){
+        if(xobj.readyState == 4 && xobj.status=='200'){
+            arrayTw= JSON.parse (xobj.responseText);
+            console.log(arrayTw);
+            crearPost();
+        }
+    }
+    xobj.send(null);
+    
     //console.log(usuario);
     //document.getElementById("usuario").innerHTML="Hola "+usuario.nombre;
     let txtTweet=document.getElementById("txtTweet");
@@ -40,7 +53,26 @@ window.onload=()=>{
                </div>
            </div>
        </div>` ;
+       el.comentarios.forEach(comentario=>{
+        todo+=` <div class="comentarios">
+        <div>
+            <img src="img/${comentario.img}" alt="" class="imgUser">
+        </div>
+        <div>
+            <h2> 
+                <span>${comentario.nombre}</span>
+                <span>${comentario.username}</span>
+            </h2>
+            <div class="textTweet">
+                ${comentario.comentario}
+            </div>
+        </div>
+    </div>` ;
+        });
+        todo+=` </div>
+        </div>¨`;
         });
         document.getElementById("posts").innerHTML=todo;
+        document.getElementById("comentarios").innerHTML=todo;
     }
 };
